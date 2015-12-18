43edbd80551439f12af8caa524faf0f52788e6da
Predrag Josifovic <pjosifovic@yahoo.com>
2015-12-17 21:53:12 -0800

    adding localStorage to market analysis

--- js/bus_mall.js
+++ js/bus_mall.js
@@ -1,34 +1,14 @@
-var clearEl = document.getElementById('clear');
-var totalEl = document.getElementById('totals');
-var barEl = document.getElementById("myChart").getContext('2d');
-var myBarChart;
-var allProducts = [];
-var productNames = [
-    'bag',
-    'banana',
-    'boots',
-    'chair',
-    'cthulhu',
-    'dragon',
-    'pen',
-    'scissors',
-    'shark',
-    'sweep',
-    'unicorn',
-    'usb',
-    'water_can',
-    'wine_glass'
-];
-
+var productNames = ["bag.jpg", "banana.jpg","boots.jpg","chair.jpg","cthulhu.jpg","dragon.jpg","pen.jpg", "scissors.jpg", "shark.jpg", "sweep.jpg", "unicorn.jpg", "usb.jpg", "water_can.jpg","wine_glass.jpg"];
 var barData = {
-    labels: [],
-    datasets: [
-        {
-            label: "My First Chart",
-            fillColor: "#1F8A70",
-            highlightFill: "#FFE11A",
-            data: []
-        }]
+	labels : [],
+	datasets : [
+		{
+      fillColor: "#1F8A70",
+      highlightFill: "#FFE11A",
+			strokeColor : "#48A4D1",
+			data : []
+		},
+	]
 };
 
 var barOptions = {
@@ -37,72 +17,69 @@ var barOptions = {
      barValueSpacing : 1,
 };
 
-function Product(name, path) {
-  this.name = name;
-  this.path = path;
+var allProducts =[];
+var myBarChartEl = document.getElementById("myChart");
+
+function Product (place){
+  this.path = "img/"+ productNames[place];
+  this.name = productNames[place].slice(0,-4);
   this.tally = 0;
-  allProducts.push(this);
-  barData.labels.push(name);
+  this.views = 0;
+  barData.labels.push(this.name);
 };
 
-
-(function buildAlbum() {
+function createAlbum(){
   for (var i = 0; i < productNames.length; i++) {
-    new Product(productNames[i], 'img/' + productNames[i] + '.jpg');
-  };
-})();
-
-
-function generateBarData() {
-  if (productRank.totalClicks % 15 === 0) {
-  for (var i = 0; i < allProducts.length; i++){
-  barData.datasets[0].data.push(allProducts[i].tally);
-  };
+    var photo = new Product(i);
+    allProducts.push(photo);
   };
 };
+
+createAlbum();
 
 var productRank = {
+  totalClicks: 0,
   leftObj: null,
   midObj: null,
   rightObj: null,
-  totalClicks: 0,
+  barChart: null,
 
-  leftEl: document.getElementById('img1'),
-  midEl: document.getElementById('img2'),
-  rightEl: document.getElementById('img3'),
-  resultsEl: document.getElementById('results'),
-  clearEl: document.getElementById('clear'),
-  imageDisplayEl: document.getElementById('imageDisplay'),
-
-  // myBarChartEl: document.getElementById('chartSection'),
+  resultsEl: document.getElementById("results"),
+  leftEl: document.getElementById("img1"),
+  midEl: document.getElementById("img2"),
+  rightEl: document.getElementById("img3"),
 
   getRandomIndex: function() {
-    return Math.floor(Math.random() * productNames.length);
+    return Math.floor(Math.random() * allProducts.length);
   },
 
-
-  displayImages: function() {
+  displayImages: function () {
     productRank.leftObj = allProducts[productRank.getRandomIndex()];
     productRank.midObj = allProducts[productRank.getRandomIndex()];
     productRank.rightObj = allProducts[productRank.getRandomIndex()];
 
-    if (productRank.leftObj === productRank.midObj || productRank.leftObj === productRank.rightObj || productRank.midObj === productRank.rightObj) {
-      productRank.displayImages();
-    };
-
-    productRank.leftEl.src = productRank.leftObj.path;
-    productRank.midEl.src = productRank.midObj.path;
-    productRank.rightEl.src = productRank.rightObj.path;
+    if(productRank.leftObj === productRank.midObj || productRank.leftObj === productRank.rightObj || productRank.midObj === productRank.rightObj) {
+    productRank.displayImages();
+    } else{
+      productRank.leftObj.views += 1;
+      productRank.midObj.views += 1;
+      productRank.rightObj.views += 1;
+    }
+     productRank.leftEl.src = productRank.leftObj.path;
+     productRank.leftEl.id = productRank.leftObj.name;
+     productRank.midEl.src = productRank.midObj.path;
+     productRank.midEl.id = productRank.midObj.name;
+     productRank.rightEl.src = productRank.rightObj.path;
+     productRank.rightEl.id = productRank.rightObj.name;
   },
 
-  showResults: function() {
+  showResults: function(){
     if (productRank.totalClicks % 15 === 0) {
-      productRank.resultsEl.disabled = false;
-      productRank.resultsEl.className = "yellow";
+    productRank.resultsEl.hidden = false;
     } else {
-      productRank.resultsEl.disabled = true;
-      productRank.resultsEl.className = "";
-      };
+    productRank.resultsEl.hidden = true;
+      myBarChartEl.hidden = true;
+    }
   }
 };
 
@@ -114,17 +91,12 @@ function compare(a,b) {
   return 0;
 };
 
-productRank.resultsEl.addEventListener("click", function(event){
-  barData.datasets[0].data = [];
-  allProducts.sort(compare);
-  generateBarData();
-  myBarChart = new Chart(barEl).Bar(barData, barOptions);
-});
+productRank.displayImages();
 
 productRank.leftEl.addEventListener('click', function() {
   productRank.leftObj.tally += 1;
   productRank.totalClicks += 1;
-  console.log('TOTAL number of clicks is ' + productRank.totalClicks);
+  console.log('Total number of clicks is ' + productRank.totalClicks);
   productRank.showResults();
   productRank.displayImages();
 });
@@ -132,7 +104,7 @@ productRank.leftEl.addEventListener('click', function() {
 productRank.midEl.addEventListener('click', function() {
   productRank.midObj.tally += 1;
   productRank.totalClicks += 1;
-  console.log('TOTAL number of clicks is ' + productRank.totalClicks);
+  console.log('Total number of clicks is ' + productRank.totalClicks);
   productRank.showResults();
   productRank.displayImages();
 });
@@ -140,14 +112,32 @@ productRank.midEl.addEventListener('click', function() {
 productRank.rightEl.addEventListener('click', function() {
   productRank.rightObj.tally += 1;
   productRank.totalClicks += 1;
-  console.log('TOTAL number of clicks is ' + productRank.totalClicks);
+  console.log('Total number of clicks is ' + productRank.totalClicks);
   productRank.showResults();
   productRank.displayImages();
 });
 
-productRank.imageDisplayEl.addEventListener('click', function() {
-    if (myBarChart !== undefined)
-    myBarChart.destroy();
-});
+var barChart = document.getElementById("myChart").getContext("2d");
 
-productRank.displayImages();
+function generateChart()  {
+  myBarChartEl.hidden = false;
+  allProducts.sort(compare);
+  for(var i=0; i < allProducts.length; i++){
+    barData.datasets[0].data[i] = allProducts[i].tally;
+  };
+    if(localStorage.tallies){
+      var barTallies = JSON.parse(localStorage.tallies);
+      for(var i=0; i<allProducts.length; i++){
+        barData.datasets[0].data[i] += barTallies[i];
+      };
+      localStorage.setItem("tallies",  JSON.stringify(barData.datasets[0].data));
+    }else{
+      localStorage.setItem("tallies", JSON.stringify(barData.datasets[0].data));
+  }
+  new Chart(barChart).Bar(barData, barOptions);
+}
+
+productRank.resultsEl.addEventListener('click', function(event) {
+  event.preventDefault();
+  generateChart();
+});
